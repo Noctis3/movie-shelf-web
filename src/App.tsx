4e2ebc78287c2e7888f4f5d212463ed0f560238f
Luan Accioly <acciolyluan@gmail.com>
2023-07-01 16:13:59 -0300

    refactor: remove colorModeSwitch from app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from './styles/theme';
-import { ColorModeSwitcher } from './components/ColorModeSwitcher';
 import { routes as RoutesReact } from './routes';
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <ColorModeSwitcher />
     <RoutesReact />
   </ChakraProvider>
 );
