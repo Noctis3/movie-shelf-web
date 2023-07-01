ccf816b7bfc9165af55966092bc630662e79ba12
Luan Accioly <acciolyluan@gmail.com>
2023-07-01 16:09:44 -0300

    feat(routes): move routes to routes file

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import theme from "./styles/theme";
-import { ColorModeSwitcher } from "./components/ColorModeSwitcher";
-
-import { Home } from "./pages/Home";
-import { Login } from "./pages/Login";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from './styles/theme';
+import { ColorModeSwitcher } from './components/ColorModeSwitcher';
+import { routes as RoutesReact } from './routes';
 export const App = () => (
   <ChakraProvider theme={theme}>
     <ColorModeSwitcher />
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    <RoutesReact />
   </ChakraProvider>
 );
