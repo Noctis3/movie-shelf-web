a602a0f43f946b28d50366d94d8d78baf06894bf
Luan Accioly <acciolyluan@gmail.com>
2023-07-03 20:36:31 -0300

    request attempt

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,16 @@
-import { Heading } from "@chakra-ui/react";
+import { Heading } from '@chakra-ui/react';
+import api from '../services/api';
+import { useEffect } from 'react';
 
 export const Home = () => {
+  api
+    .get('/movie/upcoming')
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.error('Error fetching movies:', error);
+    });
+
   return <Heading>Olá</Heading>;
 };
