4cce71b6c9c115b7e0dfff844532a8c3283c283a
Luan Accioly <acciolyluan@gmail.com>
2023-07-04 00:05:08 -0300

    feat(signin): add login request

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
   Box,
   Button,
@@ -10,8 +11,44 @@ import {
   Stack,
   useColorModeValue,
 } from '@chakra-ui/react';
+import api from '../services/api';
+import {
+  CREATE_REQUEST_TOKEN,
+  CREATE_SESSION,
+  GET_ACCOUNT_DETAILS,
+  VALIDATE_REQUEST_TOKEN,
+} from '../types/requests';
 
 export const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function handleSignIn() {
+    try {
+      const createRequestTokenResponse = await api.get(CREATE_REQUEST_TOKEN);
+      console.log(createRequestTokenResponse.data);
+      const validateRequestTokenResponse = await api.post(
+        VALIDATE_REQUEST_TOKEN,
+        {
+          username,
+          password,
+          request_token: createRequestTokenResponse.data.request_token,
+        }
+      );
+      console.log(validateRequestTokenResponse.data);
+      const createSessionResponse = await api.post(CREATE_SESSION, {
+        request_token: validateRequestTokenResponse.data.request_token,
+      });
+      console.log(createSessionResponse.data);
+      const accountDetails = await api.get(
+        `${GET_ACCOUNT_DETAILS}?session_id=${createSessionResponse.data.session_id}`
+      );
+      console.log(accountDetails.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   const moviedbRegisterLink = 'https://www.themoviedb.org/signup';
   return (
     <Flex minH={'100vh'} align={'center'} justify={'center'}>
@@ -29,11 +66,21 @@ export const Login = () => {
           <Stack spacing={4}>
             <FormControl id="email">
               <FormLabel>Email</FormLabel>
-              <Input type="email" />
+              <Input
+                onChange={(e) => {
+                  setUsername(e.target.value);
+                }}
+                type="email"
+              />
             </FormControl>
             <FormControl id="password">
               <FormLabel>Senha</FormLabel>
-              <Input type="password" />
+              <Input
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+                type="password"
+              />
             </FormControl>
             <Stack spacing={10}>
               <Link color={'blue.400'} href={moviedbRegisterLink} isExternal>
@@ -45,6 +92,7 @@ export const Login = () => {
                 _hover={{
                   bg: 'blue.500',
                 }}
+                onClick={handleSignIn}
               >
                 Entrar
               </Button>
