1961f76c80848343a77a9aeb3dc63959fe24a552
Luan Accioly <acciolyluan@gmail.com>
2023-07-05 15:51:30 -0300

    fix(api-consumption): adjust env variable name

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_API_BASE_URL;
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 console.log(BASE_URL, API_KEY);
