9a014ee1e9ea4cb8007f23cbc6443371ab8f0be3
Luan Accioly <acciolyluan@gmail.com>
2023-07-17 22:33:09 -0300

    feat: add private routes to redirect user

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,17 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Login } from './pages/Login';
+import PrivateRoute from './PrivateRoutes';
 
 export const routes = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+      <PrivateRoute>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </PrivateRoute>
     </BrowserRouter>
   );
 };
