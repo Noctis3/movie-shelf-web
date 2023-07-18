112cce47642031cd4cb4b9b9e9b41abf3298b329
Luan Accioly <acciolyluan@gmail.com>
2023-07-18 02:48:47 -0300

    feat: add request to get movie list

--- src/types/requests.ts
+++ src/types/requests.ts
@@ -3,3 +3,4 @@ export const VALIDATE_REQUEST_TOKEN =
   'authentication/token/validate_with_login';
 export const CREATE_SESSION = 'authentication/session/new';
 export const GET_ACCOUNT_DETAILS = 'account';
+export const GET_MOVIE_LIST = 'discover/movie';
