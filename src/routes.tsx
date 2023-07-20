5e10b34ff34bcfb4f8badcf0db21fd8f6dfee04d
Luan Accioly <acciolyluan@gmail.com>
2023-07-20 02:02:12 -0300

    feat: add route to Search page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Login } from './pages/Login';
+import { MovieSearch } from './pages/Search';
 import PrivateRoute from './PrivateRoutes';
 
 export const routes = () => {
@@ -10,6 +11,7 @@ export const routes = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/search" element={<MovieSearch />} />
         </Routes>
       </PrivateRoute>
     </BrowserRouter>
