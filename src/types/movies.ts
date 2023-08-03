6da65ce8a801a30f0032ba0b2196338ed56cb998
Luan Accioly <acciolyluan@gmail.com>
2023-08-03 20:46:04 -0300

    feat: add VideoData interface

--- src/types/movies.ts
+++ src/types/movies.ts
@@ -94,6 +94,17 @@ export interface CrewData {
   job: string;
 }
 
+export interface VideoData {
+  id: string;
+  iso_639_1: string;
+  iso_3166_1: string;
+  key: string;
+  name: string;
+  site: string;
+  size: number;
+  type: string;
+}
+
 export const genres = [
   {
     id: 28,
