922aaea7883ad153ee6214252791fe42a08b11f0
Luan Accioly <acciolyluan@gmail.com>
2023-08-21 02:23:31 -0300

    feat(i18n): add logic to send language on request

--- src/types/requests.ts
+++ src/types/requests.ts
@@ -1,3 +1,5 @@
+import { useTranslation } from 'react-i18next';
+
 export const CREATE_REQUEST_TOKEN = 'authentication/token/new';
 export const VALIDATE_REQUEST_TOKEN =
   'authentication/token/validate_with_login';
@@ -5,16 +7,27 @@ export const CREATE_SESSION = 'authentication/session/new';
 export const GET_ACCOUNT_DETAILS = 'account';
 export const GET_MOVIE_LIST = 'discover/movie';
 
+const languageMap: { [key: string]: string } = {
+  pt: 'pt-BR',
+  es: 'es',
+  en: 'en-US',
+};
+
+function getSelectedLanguage(language: string) {
+  return languageMap[language] || language;
+}
 export function getUserPicture(path: string | null) {
   return `https://image.tmdb.org/t/p/w400${path}`;
 }
 
-export function searchMovies(query: string) {
-  return `search/movie?query=${query}&language=pt-BR`;
+export function searchMovies(query: string, language: string) {
+  const selectedLanguage = getSelectedLanguage(language);
+  return `search/movie?query=${query}&language=${selectedLanguage}`;
 }
 
-export function getMovieDetails(id: string) {
-  return `movie/${id}?language=pt-BR`;
+export function getMovieDetails(id: string, language: string) {
+  const selectedLanguage = getSelectedLanguage(language);
+  return `movie/${id}?language=${selectedLanguage}`;
 }
 
 export function getMovieBanner(backdrop_path: string) {
@@ -24,29 +37,33 @@ export function getMovieBanner(backdrop_path: string) {
 export function getMoviePoster(poster_path: string) {
   return `https://image.tmdb.org/t/p/w400${poster_path}`;
 }
-export function getProviders(id: string) {
-  return `movie/${id}/watch/providers?language=pt-BR`;
+export function getProviders(id: string, language: string) {
+  const selectedLanguage = getSelectedLanguage(language);
+  return `movie/${id}/watch/providers?language=${selectedLanguage}`;
 }
 export function getProvidersLogo(path: string) {
   return `https://image.tmdb.org/t/p/original${path}`;
 }
 
-export function getCredits(id: string) {
-  return `movie/${id}/credits?language=pt-BR`;
+export function getCredits(id: string, language: string) {
+  const selectedLanguage = getSelectedLanguage(language);
+  return `movie/${id}/credits?language=${selectedLanguage}`;
 }
 
 export function getActorImage(path: string) {
   return `https://image.tmdb.org/t/p/w400${path}`;
 }
 
-export function getFavorites(user_id: number) {
-  return `account/${user_id}/favorite/movies?language=pt-BR`;
+export function getFavorites(user_id: number, language: string) {
+  const selectedLanguage = getSelectedLanguage(language);
+  return `account/${user_id}/favorite/movies?language=${selectedLanguage}`;
 }
 
 export function addFavorite(user_id: number) {
   return `account/${user_id}/favorite`;
 }
 
-export function getVideos(id: string) {
-  return `movie/${id}/videos?language=pt-BR`;
+export function getVideos(id: string, language: string) {
+  const selectedLanguage = getSelectedLanguage(language);
+  return `movie/${id}/videos?language=${selectedLanguage}`;
 }
